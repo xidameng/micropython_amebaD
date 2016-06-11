7fed57ae7b46e04ba82654a7166d45e5ea95ed7f
ChesterTseng <cwyark@gmail.com>
2016-06-11 20:23:36 +0800

    rlt8195a: enlarge FATFS block size to 4096
    ameba's flash api can only erase 4096 and every writing operation need
    be erased. So enlarge FATFS block size to 4096

--- rlt8195a/mphelper/flash.h
+++ rlt8195a/mphelper/flash.h
@@ -4,8 +4,10 @@ extern const struct _mp_obj_type_t pyb_flash_type;
 
 // FLASH BLOCK SIZE * FLASH_NUM_BLOCKS = RESRVED USED SIZE
 #define FLASH_START_BASE    (0xB5000)
-#define FLASH_BLOCK_SIZE    (512)
+//#define FLASH_BLOCK_SIZE    (512)
+#define FLASH_BLOCK_SIZE    (4096)
 #define FLASH_START_BLOCK   (0)
-#define FLASH_NUM_BLOCKS    (600)
+#define FLASH_NUM_BLOCKS    (75)
+//#define FLASH_NUM_BLOCKS    (600)
 
 void flash_init0(fs_user_mount_t *vfs);
