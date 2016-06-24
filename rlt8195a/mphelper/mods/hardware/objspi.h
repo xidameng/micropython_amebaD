589e30adb1245f89ef89a8c99a038623a9b66af1
ChesterTseng <cwyark@gmail.com>
2016-06-24 20:25:17 +0800

    rlt8195a: add spi module

---
+++ rlt8195a/mphelper/mods/hardware/objspi.h
@@ -0,0 +1,54 @@
+/*
+ * This file is part of the Micro Python project, http://micropython.org/
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016 Chester Tseng
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+#ifndef OBJSPI_H_
+#define OBJSPI_H_
+
+#include "spi_api.h"
+#include "spi_ex_api.h"
+
+#define SPI_MIN_BAUD_RATE                (4800)
+#define SPI_DEFAULT_BAUD_RATE            (9600)
+#define SPI_MAX_BAUD_RATE                (115200)
+
+#define SPI_MASTER                      (0)
+#define SPI_SLAVE                       (1)
+
+#define SPI_MSB                         (0)
+#define SPI_LSB                         (1)
+
+extern const mp_obj_type_t spi_type;
+
+typedef struct {
+    mp_obj_base_t base;
+    void      *obj;
+    uint8_t   id;
+    uint8_t   bits;
+    uint8_t   trans_mode;
+    uint8_t   spi_mode;
+    uint32_t  baudrate;
+} spi_obj_t;
+
+#endif  // OBJSPI_H_
