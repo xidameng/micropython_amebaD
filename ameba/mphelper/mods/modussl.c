1aa5055513ea3a81eb11732697b8dfd204a96bff
ChesterTseng <cwyark@gmail.com>
2017-06-29 17:31:01 +0800

    ameba: add cert and key in ssl module

--- ameba/mphelper/mods/modussl.c
+++ ameba/mphelper/mods/modussl.c
@@ -45,9 +45,9 @@ STATIC const mp_obj_type_t ussl_socket_type;
 typedef struct _mp_obj_ussl_socket_t {
     mp_obj_base_t base;
     mp_obj_t sock;
-    ssl_context *ssl;
-    byte *buf;
-    uint32_t bytes_left;
+    ssl_context ssl;
+    x509_crt cert;
+    pk_context pkey;
 } mp_obj_ussl_socket_t;
 
 /*****************************************************************************
@@ -55,23 +55,31 @@ typedef struct _mp_obj_ussl_socket_t {
  * ***************************************************************************/
 
 int ussl_read_wrapper(mp_obj_t *ctx, unsigned char *buf, size_t len) {
-    int16_t errcode = 0;
-    mp_uint_t ret = 0;
-    mp_obj_t obj = *ctx;
-    mp_obj_type_t *type = mp_obj_get_type(obj);
-    mp_stream_p_t *stream = type->protocol;
-    ret = stream->read(obj, buf, len, &errcode);
-    return ret;
+    mp_obj_t sock = *(mp_obj_t*)ctx;
+
+    const mp_stream_p_t *sock_stream = mp_get_stream_raise(sock, MP_STREAM_OP_READ);
+    int err;
+
+    int out_sz = sock_stream->read(sock, buf, len, &err);
+    if (out_sz == MP_STREAM_ERROR) {
+        return -err;
+    } else {
+        return out_sz;
+    }
 }
 
 int ussl_write_wrapper(mp_obj_t *ctx, const unsigned char *buf, size_t len) {
-    int16_t errcode = 0;
-    mp_uint_t ret = 0;
-    mp_obj_t obj = *ctx;
-    mp_obj_type_t *type = mp_obj_get_type(obj);
-    mp_stream_p_t *stream = type->protocol;
-    ret = stream->write(obj, buf, len, &errcode);
-    return ret;
+    mp_obj_t sock = *(mp_obj_t*)ctx;
+
+    const mp_stream_p_t *sock_stream = mp_get_stream_raise(sock, MP_STREAM_OP_WRITE);
+    int err;
+
+    int out_sz = sock_stream->write(sock, buf, len, &err);
+    if (out_sz == MP_STREAM_ERROR) {
+        return -err;
+    } else {
+        return out_sz;
+    }
 }
 
 void *ussl_mem_malloc_wrapper(size_t size) {
@@ -82,37 +90,10 @@ void ussl_mem_free_wrapper(void *ptr) {
     gc_free(ptr);
 }
 
-
-static unsigned int arc4random(void) {
-    unsigned int res = xTaskGetTickCount();
-    static unsigned int seed = 0xDEADB00B;
-    seed = ((seed & 0x007F00FF) << 7) ^
-        ((seed & 0x0F80FF00) >> 8) ^ // be sure to stir those low bits
-        (res << 13) ^ (res >> 9);    // using the clock too!
-    return seed;
-}
-
-static void get_random_bytes(void *buf, size_t len) {
-    unsigned int ranbuf;
-    unsigned int *lp;
-    int i, count;
-    count = len / sizeof(unsigned int);
-    lp = (unsigned int *) buf;
-
-    for(i = 0; i < count; i ++) {
-        lp[i] = arc4random();
-        len -= sizeof(unsigned int);
-    }
-
-    if(len > 0) { 
-        ranbuf = arc4random();
-        memcpy(&lp[i], &ranbuf, len);
-    }
-}
-
-static int my_random(void *p_rng, unsigned char *output, size_t output_len) {
-    get_random_bytes(output, output_len);
-    return 0;
+static int _random_func(void *p_rng, unsigned char *output, size_t output_len)
+{
+	rtw_get_random_bytes(output, output_len);
+	return 0;
 }
 
 STATIC void ussl_socket_print(const mp_print_t *print, mp_obj_t self_in, mp_print_kind_t kind) {
@@ -122,10 +103,12 @@ STATIC void ussl_socket_print(const mp_print_t *print, mp_obj_t self_in, mp_prin
 }
 
 STATIC mp_obj_t ussl_socket_make_new(const mp_obj_type_t *type, mp_uint_t n_args, mp_uint_t n_kw, const mp_obj_t *all_args) {
-    enum { ARG_socket, ARG_mode };
+    enum { ARG_socket, ARG_mode, ARG_key, ARG_cert};
     STATIC const mp_arg_t ussl_init_args[] = {
-        { MP_QSTR_socket, MP_ARG_REQUIRED | MP_ARG_OBJ, {.u_obj = MP_OBJ_NULL} },
-        { MP_QSTR_mode,   MP_ARG_KW_ONLY | MP_ARG_INT,  {.u_int = SSL_IS_CLIENT} },
+        { MP_QSTR_socket,  MP_ARG_REQUIRED | MP_ARG_OBJ,  {.u_obj = MP_OBJ_NULL} },
+        { MP_QSTR_mode,    MP_ARG_KW_ONLY | MP_ARG_INT,   {.u_int = SSL_IS_CLIENT} },
+        { MP_QSTR_key,     MP_ARG_KW_ONLY | MP_ARG_OBJ,   {.u_obj = MP_OBJ_NULL} },
+        { MP_QSTR_cert,    MP_ARG_KW_ONLY | MP_ARG_OBJ,   {.u_obj = MP_OBJ_NULL} },
     };
     int ret = 0;
     mp_map_t kw_args;
@@ -136,23 +119,43 @@ STATIC mp_obj_t ussl_socket_make_new(const mp_obj_type_t *type, mp_uint_t n_args
     mp_obj_ussl_socket_t *o = m_new_obj(mp_obj_ussl_socket_t);
     o->base.type = &ussl_socket_type;
     o->sock = args[ARG_socket].u_obj;
-    o->ssl = m_new_obj(ssl_context);
     memory_set_own(ussl_mem_malloc_wrapper, ussl_mem_free_wrapper);
-    if (ret = ssl_init(o->ssl) != 0)
+    if (ret = ssl_init(&o->ssl) != 0)
         mp_raise_OSError(ret);
-    ssl_set_endpoint(o->ssl, args[ARG_mode].u_int);
-    ssl_set_authmode(o->ssl, SSL_VERIFY_NONE);
-    ssl_set_rng(o->ssl, my_random, NULL);
-    ssl_set_bio(o->ssl, ussl_read_wrapper, &o->sock, ussl_write_wrapper, &o->sock);
-    if ((ret = ssl_handshake(o->ssl)) != 0) 
+    x509_crt_init(&o->cert);
+    pk_init(&o->pkey);
+    ssl_set_endpoint(&o->ssl, args[ARG_mode].u_int);
+    ssl_set_authmode(&o->ssl, SSL_VERIFY_NONE);
+    ssl_set_rng(&o->ssl, _random_func, NULL);
+    ssl_set_bio(&o->ssl, ussl_read_wrapper, &o->sock, ussl_write_wrapper, &o->sock);
+
+    if (args[ARG_key].u_obj != MP_OBJ_NULL) {
+        size_t key_len;
+        const byte *key = (const byte*)mp_obj_str_get_data(args[ARG_key].u_obj, &key_len);
+        // len should include terminating null
+        ret = pk_parse_key(&o->pkey, key, key_len + 1, NULL, 0);
+        assert(ret == 0);
+
+        size_t cert_len;
+        const byte *cert = (const byte*)mp_obj_str_get_data(args[ARG_cert].u_obj, &cert_len);
+        // len should include terminating null
+        ret = x509_crt_parse(&o->cert, cert, cert_len + 1);
+        assert(ret == 0);
+
+        ret = ssl_set_own_cert(&o->ssl, &o->cert, &o->pkey);
+        assert(ret == 0);
+    }
+
+    if ((ret = ssl_handshake(&o->ssl)) != 0) 
         mp_raise_OSError(ret);
     return o;
 }
 
 STATIC mp_obj_t socket_close(mp_obj_t self_in) {
     mp_obj_ussl_socket_t *self = self_in;
-    ssl_free(self->ssl);
-    m_del_obj(ssl_context, self->ssl);
+    ssl_free(&self->ssl);
+    x509_crt_free(&self->cert);
+    pk_free(&self->pkey);
     return mp_stream_close(self->sock);
 }
 STATIC MP_DEFINE_CONST_FUN_OBJ_1(socket_close_obj, socket_close);
@@ -168,32 +171,31 @@ STATIC MP_DEFINE_CONST_DICT(ussl_socket_locals_dict, ussl_socket_locals_dict_tab
 
 STATIC mp_uint_t ussl_socket_read(mp_obj_t self_in, void *buf, mp_uint_t size, int *errcode) {
     mp_obj_ussl_socket_t *o = self_in;
-    mp_int_t r = ssl_read(o->ssl, buf, size);
-    if (r < 0) {
-        if (r == POLARSSL_ERR_SSL_PEER_CLOSE_NOTIFY) 
-            return 0;
-        *errcode = r;
-        return MP_STREAM_ERROR;
+    mp_int_t r = ssl_read(&o->ssl, buf, size);
+    if (r == POLARSSL_ERR_SSL_PEER_CLOSE_NOTIFY)
+        return 0;
+    if (r >= 0) {
+        return r;
     }
-    return r;
+    *errcode = r;
+    return MP_STREAM_ERROR;
 }
 
 STATIC mp_uint_t ussl_socket_write(mp_obj_t self_in, const void *buf, mp_uint_t size, int *errcode) {
     mp_obj_ussl_socket_t *o = self_in;
-    mp_int_t r = ssl_write(o->ssl, buf, size);
-    if (r < 0) {
-        if (r == POLARSSL_ERR_SSL_PEER_CLOSE_NOTIFY) 
-            return 0;
-        *errcode = r;
-        return MP_STREAM_ERROR;
+    mp_int_t r = ssl_write(&o->ssl, buf, size);
+    if (r == POLARSSL_ERR_SSL_PEER_CLOSE_NOTIFY)
+        return 0;
+    if (r >= 0) {
+        return r;
     }
-    return r;
+    *errcode = r;
+    return MP_STREAM_ERROR;
 }
 
 STATIC const mp_stream_p_t ussl_socket_stream_p = {
     .read = ussl_socket_read,
     .write = ussl_socket_write,
-    .is_text = false,
 };
 
 STATIC const mp_obj_type_t ussl_socket_type = {
